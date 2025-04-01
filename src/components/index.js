8fabe06e0ccdd7bb27f6eaeacd10f24ff9c97d07
Karan Singh <karans21.dev@gmail.com>
2025-04-01 14:55:30 +0530

    .

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./Container.jsx";
+import Container from "./Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
@@ -12,6 +12,7 @@ import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
 import Input from "./Input";
+// Removed unused import 'index'
 export {
   Header,
   Footer,
